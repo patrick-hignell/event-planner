2b1b0fe6604b9d02e63c7a10a5026cc27c485ead
Patrick Hignell <patrickhignell@hotmail.com>
2025-11-12 12:11:15 +1300

    add arrayofarray

--- client/components/Schedule.tsx
+++ client/components/Schedule.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 import { Event, TimeSlot } from '../../models/events'
 
 function Schedule() {
-  const [schedule, setSchedule] = useState<TimeSlot[]>([])
+  const [schedule, setSchedule] = useState<TimeSlot[][]>([[]])
 
   const {
     data: events,
@@ -19,12 +19,9 @@ function Schedule() {
       events.forEach((event) => {
         timeSlots.push(...eventToTimeSlot(event))
       })
-      timeSlots.sort(
-        (a, b) =>
-          Number(new Date(`${a.day} ${a.startTime}:00`)) -
-          Number(new Date(`${b.day} ${b.startTime}:00`)),
-      )
-      setSchedule([...timeSlots])
+      const orderedTimeSlots: TimeSlot[][] = orderTimeSlots([...timeSlots])
+      console.log(orderedTimeSlots)
+      setSchedule([...orderedTimeSlots])
     }
   }, [events])
 
@@ -46,6 +43,41 @@ function Schedule() {
     return tempTimeSlots
   }
 
+  function orderTimeSlots(array: TimeSlot[]) {
+    const arrayOfArrays: TimeSlot[][] = [[]]
+    array.sort(
+      (a, b) =>
+        Number(new Date(`${a.day} ${a.startTime}:00`)) -
+        Number(new Date(`${b.day} ${b.startTime}:00`)),
+    )
+    arrayOfArrays[0].push(array[0])
+    for (let i = 1; i < array.length; i++) {
+      for (let j = 0; j < arrayOfArrays.length; j++) {
+        const latestTimeSlot = arrayOfArrays[j][arrayOfArrays[j].length - 1]
+        console.log(
+          Number(new Date(`${array[i].day} ${array[i].startTime}:00`)),
+        )
+        console.log(
+          Number(
+            new Date(`${latestTimeSlot.day} ${latestTimeSlot.endTime}:00`),
+          ),
+        )
+        if (
+          Number(new Date(`${array[i].day} ${array[i].startTime}:00`)) >
+          Number(new Date(`${latestTimeSlot.day} ${latestTimeSlot.endTime}:00`))
+        ) {
+          arrayOfArrays[j].push(array[i])
+          break
+        }
+        if (j === arrayOfArrays.length - 1) {
+          arrayOfArrays.push([array[i]])
+          break
+        }
+      }
+    }
+    return arrayOfArrays
+  }
+
   if (isPending) {
     return <p>loading...</p>
   }
@@ -55,16 +87,20 @@ function Schedule() {
   }
 
   return (
-    <div>
+    <div className="flex flex-row p-3 gap-2">
       <h1>Schedule</h1>
-      {schedule.map((slot) => (
-        <div key={`${slot.eventId}${slot.day}${slot.startTime}`}>
-          <p>Day: {slot.day}</p>
-          <p>Start Time: {slot.startTime}</p>
-          <p>End Time: {slot.endTime}</p>
-          <p>Name: {slot.name}</p>
-          <p>Location: {slot.location}</p>
-          <br></br>
+      {schedule.map((column, index) => (
+        <div key={index} className="flex flex-col p-3 gap-2">
+          {column.map((slot) => (
+            <div key={`${Object.values(slot)}`} className="">
+              <p>Day: {slot.day}</p>
+              <p>Start Time: {slot.startTime}</p>
+              <p>End Time: {slot.endTime}</p>
+              <p>Name: {slot.name}</p>
+              <p>Location: {slot.location}</p>
+              <br></br>
+            </div>
+          ))}
         </div>
       ))}
     </div>
