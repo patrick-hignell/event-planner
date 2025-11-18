dc5e4756323591441735321fa0129d8de31c3bb0
Patrick Hignell <patrickhignell@hotmail.com>
2025-11-18 16:24:53 +1300

    change dates from string to text

---
+++ server/db/migrations/20251118032327_datestext.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.alterTable('events', function (table) {
+    table.text('dates').alter()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.alterTable('events', function (table) {
+    table.string('dates').alter()
+  })
+}
