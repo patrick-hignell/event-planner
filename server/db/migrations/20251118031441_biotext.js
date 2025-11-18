84d1678e23c67138890bd15ccce2a43ca427f972
Patrick Hignell <patrickhignell@hotmail.com>
2025-11-18 16:15:13 +1300

    change bio from string to text

---
+++ server/db/migrations/20251118031441_biotext.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.alterTable('events', function (table) {
+    table.text('bio').alter()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.alterTable('events', function (table) {
+    table.string('bio').alter()
+  })
+}
